7e05aa439661942635b4bd3c8d3dcd92da87afea
musayevjahongir <musayevjahongir5051@gmail.com>
2023-11-04 14:22:11 +0500

    idlar

--- find_all_users_id.py
+++ find_all_users_id.py
@@ -11,7 +11,10 @@ def find_all_users_id(data: dict)->list:
     """
     a=[]
     for i in data["messages"]:
-        a.append(i["id"])
+        if "actor_id" in i and i["actor_id"] not in a and "channel" not in i["actor_id"]:
+            a.append(i["actor_id"])
+        elif "from_id" in i and i["from_id"] not in a and "channel" not in i["from_id"]:
+            a.append(i["from_id"])
     return a
 s=read_data("data/result.json")
-print(find_all_users_id(s))
+print(len(find_all_users_id(s)))
